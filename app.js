f9ddebf0029b30910235e8df22aabe8450b64215
Xinyuan <xinyuan@Xinyuans-MacBook-Pro.local>
2018-12-20 13:23:10 -0600

    adding mongodb

---
+++ app.js
@@ -0,0 +1,65 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+var mongoose = require("mongoose")
+mongoose.connect("mongodb://localhost/db");
+
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+app.set("view engine", "ejs");//不用再写.ejs
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+    Campground.find({}, function(err, allcampgrounds){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("index", {campgrounds: allcampgrounds});
+        }
+    });
+    
+});
+
+app.post("/campgrounds", function(req, res){
+    //get data from form add to campground page
+    var name = req.body.name;
+    var image = req.body.image;
+    var description = req.body.description;
+    var newCampGround = {name: name, image: image, description: description};
+    Campground.create({
+        name: name, 
+        image: image,
+        description: description
+    });
+    res.redirect("/index");
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new");
+});
+
+//SHOW shows more info about this tv show
+app.get("/campgrounds/:id", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("show", {campground: foundCampground});
+        }
+    });
+    
+})
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("the server has started");
+});
